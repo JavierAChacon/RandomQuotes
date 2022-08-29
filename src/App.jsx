9eeb3dfc444c4cc24413a6f6fbd9fb3d63d61b4b
JavierChacon <alejandro.chr@outlook.es>
2022-08-29 11:10:33 -0400

    adding new colors

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   const [index, setIndex] = useState(random)
 
-  const colors = ['#104F55', '#32746D', '#9EC5AB', '#FAC9B8', '#EF476F']
+  const colors = ['#104F55', '#32746D', '#9EC5AB', '#FAC9B8', '#EF476F', '#D3F2A3', '#97E196', '#6CC08B', '#4C9B82', '#217A79', '#105965', '#074050' ]
   
   const randomColor = Math.floor(Math.random() * colors.length)  
   
