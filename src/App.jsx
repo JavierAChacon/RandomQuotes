e35dbb44a2b54e2f0e3e9f86f56ff084956c073c
JavierChacon <alejandro.chr@outlook.es>
2022-08-28 23:46:36 -0400

    firstAdvance

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
+import { useState } from 'react'
 import './App.css'
-import QuoteBox from './components/QuoteBox'
+import quotes from './quotes.json'
 
 function App() {
-  
-  const random = Math.floor(Math.random() * 20)
-  
+
+  const random = Math.floor(Math.random() * quotes.length)
+
+  const [index, setIndex] = useState(random)
+
+  const change = () => {
+    const random = Math.floor(Math.random() * quotes.length)
+    setIndex(random)
+
+
+  }
+
   return (
     <div className="App">
-     <QuoteBox
-     />
+
+      <div className='quotebox'>
+        <div className='message'>
+          <i class="quote fa-solid fa-quote-left quote"></i>
+          <p>{quotes[index].quote}</p>
+        </div>
+
+        <div className='author'>
+          <p>{quotes[index].author}</p>
+          <button onClick={change}><i class="fa-solid fa-angle-right"></i></button>
+        </div>
+
+      </div>
+
     </div>
   )
 }
 
-export default App
+export default App;
