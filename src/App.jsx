0f1ff2a68465f1cff08175ae9d86dfb70a3e3e48
JavierChacon <alejandro.chr@outlook.es>
2022-08-29 10:39:48 -0400

    matching colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,34 @@ import './App.css'
 import quotes from './quotes.json'
 
 function App() {
-
   const random = Math.floor(Math.random() * quotes.length)
 
   const [index, setIndex] = useState(random)
 
-
+  
+  
   const change = () => {
     const random = Math.floor(Math.random() * quotes.length)
     setIndex(random)
-    
   }
+  
+  const colors = ['#BAF2E9', '#AFF8DD', '#B0FCC8', '#BFFEAD', '#D8FD8F', '#F9F871']
+  
+  const randomColor = Math.floor(Math.random() * colors.length)
 
   return (
-    <div className="App">
+    <div style={{backgroundColor: `${colors[randomColor]}`}} className="App">
 
       <div className='quotebox'>
+        
         <div className='message'>
-          <i class={`quote fa-solid fa-quote-left quote`}></i>
-          <p>{quotes[index].quote}</p>
+          <i style={{color:`${colors[randomColor]}`}} class={`quote fa-solid fa-quote-left quote`}></i>
+          <p style={{color:`${colors[randomColor]}`}}>{quotes[index].quote}</p>
         </div>
 
         <div className='author'>
-          <p>{quotes[index].author}</p>
-          <button onClick={change}><i class="fa-solid fa-angle-right"></i></button>
+          <p style={{color:`${colors[randomColor]}`}}>{quotes[index].author}</p>
+          <button style={{backgroundColor: `${colors[randomColor]}`}} onClick={change}><i class="fa-solid fa-angle-right"></i></button>
         </div>
 
       </div>
