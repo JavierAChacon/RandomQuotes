7c2b01dca029a9505149c57628327256cdeb2d00
JavierChacon <alejandro.chr@outlook.es>
2022-09-01 01:20:07 -0400

    new colors

--- src/components/Colors.jsx
+++ src/components/Colors.jsx
@@ -1,3 +1,3 @@
-const colors = ['#104F55', '#32746D', '#9EC5AB', '#FAC9B8', '#EF476F', '#D3F2A3', '#97E196', '#6CC08B', '#4C9B82', '#217A79', '#105965', '#074050', '#FFC6FF', '#FFADAD', '#FFD6A5', '#FDFFB6', '#CAFFBF', '#9BF6FF', '#AOC4FF', '#BDB2FF']
+const colors = ['#264653', '#2A9D8F', '#E9C46A','#F4A261,', '#E76F51']
 
 export default colors;
